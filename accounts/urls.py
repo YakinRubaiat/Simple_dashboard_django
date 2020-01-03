a432f7aaf59e94399a172ee7947d56b1bad7fb06
YakinRubaiat <yaken.rubaiat11@gmail.com>
2020-01-03 18:05:52 +0600

    LoginView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', views.home, name='index'),
+    path('login', LoginView.as_view(template_name='accounts/login.html')),
 ]

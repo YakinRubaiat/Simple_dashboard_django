3ad7c0e97ee3fae14a2093db33a5eac0a37fe4f8
YakinRubaiat <yaken.rubaiat11@gmail.com>
2020-01-06 11:43:49 +0600

    Part 15

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.home, name='index'),
     path('login', LoginView.as_view(template_name='accounts/login.html')),
+    path('logout', LogoutView.as_view(template_name='accounts/logout.html')),
+    path('register', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home(request):
     return render(request,'accounts/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    
+    else:
+        form = UserCreationForm()
+
+        args = {'form':form}
+        return render(request, 'accounts/reg_form.html',args)
 
